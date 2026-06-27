0617294fc45fbd5833a46e9ebdf3915911315b3d
praveen <139388387+praveen200135@users.noreply.github.com>
2026-06-27 10:36:35 +0530

    practice_01

---
+++ data_type.js
@@ -0,0 +1,21 @@
+var pin= 7899999
+console.log(pin)
+console.log(typeof pin);
+
+var name= `7899999`
+console.log(name)
+console.log(typeof name);
+
+var names= true
+console.log(names)
+console.log(typeof names);
+
+var name1
+console.log(name1)
+console.log(typeof name1);
+
+var tame= null
+console.log(tame)
+console.log(typeof tame);
+
+
---
+++ odd_eve.js
@@ -0,0 +1,16 @@
+function numberfunction(num) {
+
+
+    if (num % 2 === 0) {
+        console.log("number is a even numer" + " " + num)
+    }
+
+    else {
+        console.log("numer is a odd numer" + " " + num)
+
+    }
+}
+
+for(let i=0;i<=10;i++){
+numberfunction(i)
+}
